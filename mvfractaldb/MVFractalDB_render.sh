02154753ef6bafca55a9096f08b154f27cbbaea4
Ryosuke Yamada <50004406+ryosuke-yamada@users.noreply.github.com>
2022-06-16 22:29:29 +0900

    Update MVFractalDB_render.sh

--- mvfractaldb/MVFractalDB_render.sh
+++ mvfractaldb/MVFractalDB_render.sh
@@ -2,7 +2,7 @@
 variance_threshold=0.05
 numof_category=1000
 param_path='./../MVFractalDB/3DIFS_params/MVFractalDB-'${numof_category}
-3dmodel_save_path='./../MVFractalDB/3Dmodels/MVFractalDB-'${numof_category}
+model_save_path='./../MVFractalDB/3Dmodels/MVFractalDB-'${numof_category}
 image_save_path='./../MVFractalDB/images/MVFractalDB-'${numof_category}
 
 # Parameter search
@@ -12,4 +12,4 @@ python 3dfractal_render/category_search.py --variance=${variance_threshold} --nu
 python 3dfractal_render/instance.py --load_root ${param_path} --save_root ${3dmodel_save_path} --classes ${numof_category}
 
 # Render Multi-view images
-python image_render/render.py --load_root ${3dmodel_save_path} --save_root ${image_save_path}
+python image_render/render.py --load_root ${model_save_path} --save_root ${image_save_path}
