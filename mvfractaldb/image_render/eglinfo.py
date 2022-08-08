0fecb692fe4d5543bf59c550df55796ea0565f16
Rtyota Suzuki <suzuryo3893@gmail.com>
2022-08-09 08:46:35 +0900

    add docstring

--- mvfractaldb/image_render/eglinfo.py
+++ mvfractaldb/image_render/eglinfo.py
@@ -7,6 +7,18 @@ from typing import List
 EGLDeviceEXT = ctypes.c_void_p
 
 def enumerateGraphicDevices(_maxDevices : int) -> List[EGLDeviceEXT]:
+    '''enumerateGraphicDevices
+    Enumerate available graphic devices. It includes software renderer if it is available.
+
+    Args:
+        _maxDevices : int - num of devices to enumerate
+    
+    Return:
+        List[EGLDeviceEXT] - list of devices
+    
+    Comment:
+        EGLDeviceEXT = ctypes.c_void_p
+    '''
     maxDevices = ctypes.c_int32(_maxDevices)
     PEGLDevEXT = EGLDeviceEXT * _maxDevices
     _eglDevs = [None] * _maxDevices
@@ -18,6 +30,15 @@ def enumerateGraphicDevices(_maxDevices : int) -> List[EGLDeviceEXT]:
 
 
 def printGraphicDevices(eglDevs : List[EGLDeviceEXT]) -> None:
+    '''printGraphicDevices
+    Print device name of graphic devices.
+
+    Args:
+        eglDevs : List[EGLDeviceEXT] - list of devices
+
+    Comment:
+        EGLDeviceEXT = ctypes.c_void_p
+    '''
     eglQueryDeviceStringEXT=pegl.egl._common._load_function("eglQueryDeviceStringEXT", ctypes.c_char_p, [EGLDeviceEXT, ctypes.c_int32])
     for i,d in enumerate(eglDevs):
         s = eglQueryDeviceStringEXT(d, pegl.egl.EGL_EXTENSIONS)
@@ -25,4 +46,5 @@ def printGraphicDevices(eglDevs : List[EGLDeviceEXT]) -> None:
         # EGL_NV_device_cuda
     return
 
-printGraphicDevices(enumerateGraphicDevices(10))
+if __name__=="__main__":
+    printGraphicDevices(enumerateGraphicDevices(10))
